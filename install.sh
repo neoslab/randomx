8ffaa97b5e83c3eaad410f936aac3a9b96c88af8
neoslab <github@neoslab.com>
2023-02-05 17:04:03 +0100

    Update project repository

--- install.sh
+++ install.sh
@@ -42,7 +42,3 @@ sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrad
 ## ------------------------
 sudo apt-get -y remove && sudo apt-get -y autoremove
 sudo apt-get -y clean && sudo apt-get -y autoclean
-
-## Reboot system
-## -------------
-history -c && history -w && reboot now
